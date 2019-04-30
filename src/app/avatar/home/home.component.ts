fd781a50eaef7f9bb0f92f1e7e1145f53d41cd28
cfxp <cfxp001@gmail.com>
2019-05-01 02:17:48 +0900

    .

--- src/app/avatar/home/home.component.ts
+++ src/app/avatar/home/home.component.ts
@@ -6,7 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  trends: Trend[];
+  constructor() { }
+
+  ngOnInit() {
+    this.trends = [
+      {
+        title: "oil",
+        value: 123,
+        trend: true
+      },
+      {
+        title: "machinary",
+        value: 100,
+        trend: true
+      },
+      {
+        title: "mineral",
+        value: 188,
+        trend: false
+      },
+      {
+        title: "coal",
+        value: 109,
+        trend: true
+      },
+      {
+        title: "ice",
+        value: 456,
+        trend: false
+      },
+
+    ]
+
+  }
+}
+
+export interface Trend {
+  title: string;
+  value: number;
+  trend: boolean;
 }
